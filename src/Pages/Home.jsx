efc963cdd954ddc69207427b15a4b5e3dc825eed
miftah belay <enkubelay21@gmail.com>
2026-09-17 09:22:43 +0300

    banner

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Header } from '../Component/header/Header'
 import { Footer } from '../Component/footer/Footer'
+import Banner from '../Component/Banner/Banner';
 
 export const Home = () => {
   return (
     <>
       <Header />
+      <Banner />
       <Footer />
     </>
   );
